fbcfa1fdfc23564a81099689248e1de8bce2832e
radu <rgoldentime@gmail.com>
2018-03-01 18:02:38 +0100

    Model Sobre

--- sobres/isobres/models.py
+++ sobres/isobres/models.py
@@ -4,3 +4,8 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+class Sobre(models.Model):
+    date = models.DateTimeField()
+    amount = models.IntegerField()
+    concept = models.TextField(max_length=100)
