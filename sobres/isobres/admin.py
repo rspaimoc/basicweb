a6ea32480765079aaa2e78f8b3c10bc8e44ad0f3
radu <rgoldentime@gmail.com>
2018-03-01 18:20:32 +0100

    Donor with foreign keys

--- sobres/isobres/admin.py
+++ sobres/isobres/admin.py
@@ -2,7 +2,9 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
-from isobres.models import Sobre
+from isobres.models import Sobre, Donor
 
 # Register your models here.
 admin.site.register(Sobre)
+
+admin.site.register(Donor)
--- sobres/isobres/models.py
+++ sobres/isobres/models.py
@@ -2,10 +2,17 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+
+class Donor(models.Model):
+     name = models.CharField(max_length=40)
 
 class Sobre(models.Model):
     date = models.DateTimeField()
     amount = models.IntegerField()
     concept = models.TextField(max_length=100)
+    donor = models.ForeignKey(Donor)
+    user = models.ForeignKey(User)
