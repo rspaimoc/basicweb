9f793e39e473590fd9e75bead3c1f132024b5031
radu <rgoldentime@gmail.com>
2018-03-01 17:58:16 +0100

    first view created

--- sobres/sobres/urls.py
+++ sobres/sobres/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from isobres.views import *
 
 urlpatterns = [
+    url(r'^$', mainpage, name='home'),
     url(r'^admin/', admin.site.urls),
 ]
